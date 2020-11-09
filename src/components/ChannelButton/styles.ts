078815f58756ec4a6c1fad7eebbb16adf0950852
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-11-09 20:15:04 -0300

    First Commit

---
+++ src/components/ChannelButton/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  cursor: pointer;
+  
+  padding: 5px 3px;
+`;
+
+export const HashtagIcon = styled.div``;
+
+export const InviteIcon = styled.div``;
+
+export const SettingsIcon = styled.div``;
